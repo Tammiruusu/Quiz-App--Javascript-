c9c2f11faeaed8104c1373fd40c11f81118396a0
Tiia <tiiu_maria@hotmail.com>
2026-09-02 21:15:16 +0300

    Kysymyksen rungot lisätty

---
+++ script.js
@@ -0,0 +1,29 @@
+const questions = [
+    {
+        kysymys: "Kysymys teksti?",
+        answers: [
+            { Text: "vastaus 1", correct: false},
+            { Text: "vastaus 2", correct: true},
+            { Text: "vastaus 3", correct: false},
+            { Text: "vastaus 4", correct: false}
+        ]
+    },
+     {
+        kysymys: "Kysymys teksti2?",
+        answers: [
+            { Text: "vastaus 1", correct: false},
+            { Text: "vastaus 2", correct: true},
+            { Text: "vastaus 3", correct: false},
+            { Text: "vastaus 4", correct: false}
+        ]
+    },
+     {
+        kysymys: "Kysymys teksti3?",
+        answers: [
+            { Text: "vastaus 1", correct: false},
+            { Text: "vastaus 2", correct: true},
+            { Text: "vastaus 3", correct: false},
+            { Text: "vastaus 4", correct: false}
+        ]
+    }
+];
