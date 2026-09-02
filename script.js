86fc2f134d6658d3b3de3a8d41d06b7bdb32bf6d
Tiia <tiiu_maria@hotmail.com>
2026-09-02 21:19:22 +0300

    Pelin aloitus funktio kunnossa

--- script.js
+++ script.js
@@ -27,3 +27,19 @@ const questions = [
         ]
     }
 ];
+
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-button");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
