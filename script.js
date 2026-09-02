da0d94d77feb94da66941af9cd84dce2007095bd
Tiia <tiiu_maria@hotmail.com>
2026-09-02 22:00:46 +0300

    Scriptit valmiina

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ function showQuestion() {
     resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.kysymys;
 
 
     //tällä tuodaan vastaukset näkyviin
@@ -61,7 +61,7 @@ function showQuestion() {
         const button = document.createElement("button");
         button.innerHTML = answer.Text;
         button.classList.add("btn");
-        answerButton.appendChild(button);
+        answerButtons.appendChild(button);
         //Laskee OIKEAT vastaukset
         if(answer.correct){
             button.dataset.correct = answer.correct;
@@ -80,15 +80,55 @@ function resetState() {
 }
 
 //lisätään CORRECT tai INCORRECT luokat riippuen onko vastaus OIKEIN vai VÄÄRIN!
-function selectAnswer() {
+function selectAnswer(e) {
     const selectedBtn = e.target;
     const isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect){
         selectedBtn.classList.add("correct");
+        score++;
     } else {
         selectedBtn.classList.add("incorrect");
+    } 
+    //tarkistaa, jos et painanut OIKEAA vastausta, array For Each etsii oikean vastauksen
+    //ja lisää siihen CORRECT luokan. Sekä pistää napit poist käytöstä
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = "true";
+    });
+    nextButton.style.display = "block";
+}
+
+
+//pelin lopuksi näyttää tuloksen ja antaa pelaa uudelleen napin
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `Tuloksesi on ${score} oikein kaikista ${questions.length} kysymyksestä!`;
+    nextButton.innerHTML = "Pelaa uudelleen";
+    nextButton.style.display = "block";
+}
+
+//antaa seuraavan kysymyksen TAI jos kysymykset loppu, näyttää SCOREN
+function handleNextButton() {
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    } else {
+        showScore();
     }
 }
+
+
+//tapahtuma kuuntelijan lisäys seuraavaan nappiin, odottaa CLICK tapahtumaa
+//suorittaa joko HandleNextButton tai startQuiz funtion
+nextButton.addEventListener("click", () => {
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    } else {
+        startQuiz(); 
+    }
+})
 
 
 //kutsutaan pelin aloitus funktiota lopussa. 
