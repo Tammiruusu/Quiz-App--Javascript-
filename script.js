0c269ef344e71dac8c28be975753cfaec5978b70
Tiia <tiiu_maria@hotmail.com>
2026-09-02 21:33:14 +0300

    Funktio kysymysten näyttämiseen ja pisteen laskuun kunnossa

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//kysymykset alla, niitä voi muokata/lisätä halutun määrän
+//muista merkitä CORRECT : TRUE oikeisiin vastauksiin! 
 const questions = [
     {
         kysymys: "Kysymys teksti?",
@@ -30,12 +32,14 @@ const questions = [
 
 
 const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-button");
+const answerButtons = document.getElementById("answer-button");
 const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
 let score = 0;
 
+
+//pelin aloitus funktio, palauttaa tuloksen/scoren nollaan. 
 function startQuiz() {
     currentQuestionIndex = 0;
     score = 0;
@@ -43,3 +47,35 @@ function startQuiz() {
     showQuestion();
 }
 
+//tämä näyttää kysymykset
+function showQuestion() {
+    //poistaa edelliset napit ja muut härpäkkeet
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+
+    //tällä tuodaan vastaukset näkyviin
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.Text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+    })
+}
+
+
+//funktio poistaa edelliset napit käytöstä
+function resetState() {
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+
+
+
+//kutsutaan pelin aloitus funktiota lopussa. 
+startQuiz();
